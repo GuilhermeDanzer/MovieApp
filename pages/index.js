c448030667d01d77cf4c04f3d0a2b56e65950117
GuilhermeDanzer <guidanzer@hotmail.com>
2023-01-18 03:41:32 -0300

    Add search

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
-import { useState, useRef, useCallback } from "react"
+import { useState, useRef, useEffect, useCallback } from "react"
 import styles from "../styles/Home.module.css"
-import { getMovies } from "../services/moviesServices"
+import { getMovies, search } from "../services/moviesServices"
 import ItemCard from "../components/itemCard"
 import Layout from "../layout/Layout"
 import { getGenres } from "../utils/getGenres"
@@ -9,30 +9,32 @@ export default function Home({ propsMovies, current_page, max_page }) {
   const categories = [
     {
       category: "Upcoming",
-      value: "upcoming",
+      value: "/upcoming",
       selected: false,
     },
     {
       category: "Popular",
-      value: "popular",
+      value: "/popular",
       selected: false,
     },
     {
       category: "Now Playing",
-      value: "now_playing",
+      value: "/now_playing",
       selected: false,
     },
     {
       category: "Top Rated",
-      value: "top_rated",
+      value: "/top_rated",
       selected: false,
     },
   ]
   const [movies, setMovies] = useState(propsMovies)
-  const [moviesEndpoint, setMoviesEndpoint] = useState("upcoming")
-  const [currentPage, setCurrentPage] = useState(current_page)
+  const [moviesEndpoint, setMoviesEndpoint] = useState("/upcoming")
+  const [currentPage, setCurrentPage] = useState(1)
   const [loading, setLoading] = useState(false)
   const [hasMore, setHasMore] = useState(currentPage < max_page)
+  const [searchTerm, setSearchTerm] = useState("")
+
   const observer = useRef()
   const lastMovieElementRef = useCallback(
     node => {
@@ -50,33 +52,67 @@ export default function Home({ propsMovies, current_page, max_page }) {
   const fetchMoreData = async () => {
     if (hasMore === false) return
     setLoading(loading => !loading)
-    const { movies: fetchedMovies, current_page } = await getMovies(
-      moviesEndpoint,
-      currentPage + 1
-    )
+    const { movies: fetchedMovies, current_page } = searchTerm
+      ? await search(searchTerm, currentPage + 1)
+      : await getMovies(moviesEndpoint, currentPage + 1)
 
     setMovies(movies => [...movies, ...fetchedMovies])
     setCurrentPage(currentPage => current_page)
+    setHasMore(hasMore => currentPage < max_page)
     setLoading(loading => !loading)
   }
 
-  const changeMovieEndpoint = async value => {
+  const changeMovieEndpoint = async (value = moviesEndpoint) => {
     setLoading(loading => true)
+    setMovies(movies => [])
     setMoviesEndpoint(moviesEndpoint => value)
-
+    setSearchTerm(searchTerm => "")
     const { movies: fetchedMovies } = await getMovies(value, 1)
 
     setMovies(movies => (fetchedMovies ? fetchedMovies : []))
     setCurrentPage(currentPage => 1)
     setLoading(loading => false)
   }
+
+  const fetchSearchData = async () => {
+    const { movies: fetchedMovies, current_page } = await search(searchTerm, 1)
+
+    setMovies(movies => [...fetchedMovies])
+    setCurrentPage(currentPage => current_page)
+
+    setLoading(loading => false)
+  }
+
+  useEffect(() => {
+    setMovies(movies => [])
+    setLoading(loading => true)
+
+    const delayDebounceFn = setTimeout(
+      () => {
+        searchTerm ? fetchSearchData() : changeMovieEndpoint()
+      },
+      searchTerm ? 3000 : 100
+    )
+
+    return () => clearTimeout(delayDebounceFn)
+  }, [searchTerm])
   return (
     <Layout>
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>Movies List</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <div className={styles.moviesCategoryDiv}>
+          <input
+            autoFocus
+            type="text"
+            autoComplete="off"
+            className={styles.searchBar}
+            placeholder="Search here..."
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+        </div>
         <div className={styles.moviesCategoryDiv}>
           {categories.map((item, i) => {
             return (
@@ -93,41 +129,52 @@ export default function Home({ propsMovies, current_page, max_page }) {
             )
           })}
         </div>
-        <div className={styles.cardDisplay}>
-          {movies.map((movie, index) => {
-            if (movies.length === index + 1) {
-              return (
-                <ItemCard
-                  ref={lastMovieElementRef}
-                  key={movie.id}
-                  title={movie.title}
-                  subTitle={getGenres(movie.genre_ids).join(", ")}
-                  photoPath={movie.poster_path}
-                  buttonAction={`/${movie.id}`}
-                  buttonText="See Details"
-                />
-              )
-            } else {
-              return (
-                <ItemCard
-                  key={movie.id}
-                  title={movie.title}
-                  subTitle={getGenres(movie.genre_ids).join(", ")}
-                  photoPath={movie.poster_path}
-                  buttonAction={`/${movie.id}`}
-                  buttonText="See Details"
-                />
-              )
-            }
-          })}
-        </div>
+        {(!loading && movies.length < 1) || movies.length > 0 ? (
+          <div className={styles.cardDisplay}>
+            {movies.map((movie, index) => {
+              if (movies.length === index + 1) {
+                return (
+                  <ItemCard
+                    ref={lastMovieElementRef}
+                    key={movie.id}
+                    title={movie.title}
+                    subTitle={getGenres(movie.genre_ids).join(", ")}
+                    photoPath={movie.poster_path}
+                    buttonAction={`/${movie.id}`}
+                    buttonText="See Details"
+                  />
+                )
+              } else {
+                return (
+                  <ItemCard
+                    key={movie.id}
+                    title={movie.title}
+                    subTitle={getGenres(movie.genre_ids).join(", ")}
+                    photoPath={movie.poster_path}
+                    buttonAction={`/${movie.id}`}
+                    buttonText="See Details"
+                  />
+                )
+              }
+            })}
+          </div>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: 20,
+            }}>
+            <span className={styles.loader}></span>
+          </div>
+        )}
       </div>
     </Layout>
   )
 }
 
 export async function getStaticProps() {
-  const { movies, current_page, max_page } = await getMovies("upcoming")
+  const { movies, current_page, max_page } = await getMovies("/upcoming")
 
   return {
     props: {
--- services/moviesServices.js
+++ services/moviesServices.js
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios"
-const baseURL = (endpoint, parameters = "") =>
-  `https://api.themoviedb.org/3/movie/${endpoint}?api_key=6d2e5d2be49fdfb5a407b6bdfdcaea60${parameters}`
+const baseURL = (endpoint = "", parameters = "") =>
+  `https://api.themoviedb.org/3/${endpoint}?api_key=6d2e5d2be49fdfb5a407b6bdfdcaea60${parameters}`
 
 export const getMovies = async (movieEndpoint, page = 1) => {
   const { data } = await axios.get(
-    baseURL(movieEndpoint, `&language=en_US&page=${page}`)
+    baseURL(`movie/${movieEndpoint}`, `&language=en_US&page=${page}`)
   )
   return {
     movies: data.results,
@@ -16,13 +16,26 @@ export const getMovies = async (movieEndpoint, page = 1) => {
 
 export const getMoviesCredit = async movieId => {
   const { data } = await axios.get(
-    baseURL(`${movieId}/credits`, "&language=en_US")
+    baseURL(`movie/${movieId}/credits`, "&language=en_US")
   )
 
   return { crew: data.crew, cast: data.cast }
 }
 
 export const getMovie = async movieId => {
-  const { data } = await axios.get(baseURL(movieId, "&language=en-US"))
+  const { data } = await axios.get(
+    baseURL(`movie/${movieId}`, "&language=en-US")
+  )
   return { movie: data }
+}
+
+export const search = async (query, page) => {
+  const { data } = await axios.get(
+    baseURL("search/movie", `&query=${query}&page=${page}`)
+  )
+  return {
+    movies: data.results,
+    current_page: data.page,
+    max_page: data.total_pages,
+  }
 }
